a39f6a912183158c30a01eede26018e9f497df29
Feofilaktov, Sergey <man@smairon.ru>
2024-05-26 17:36:38 +0300

    Added CQEAdapterContract

--- hermitage/definitions/contracts/adapters.py
+++ hermitage/definitions/contracts/adapters.py
@@ -1,5 +1,9 @@
 import abc
 import typing
+import collections.abc
+
+import zodchy
+from ...notation.default import InvoiceElement
 
 
 class ModifierContract(abc.ABC):
@@ -18,3 +22,11 @@ class ConstraintContract(ModifierContract, abc.ABC):
 class TransformerContract(ModifierContract, abc.ABC):
     @abc.abstractmethod
     def __call__(self, value: typing.Any) -> typing.Any: ...
+
+
+class CQEAdapterContract(typing.Protocol):
+    def __call__(
+        self,
+        message: zodchy.codex.cqea.Query | zodchy.codex.cqea.WriteEvent,
+        *modifiers: ModifierContract
+    ) -> collections.abc.Generator[InvoiceElement, None, None]: ...
