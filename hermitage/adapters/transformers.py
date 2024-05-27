29de98253734bf69459c88256d8a0d17dcfb4ee2
Feofilaktov, Sergey <man@smairon.ru>
2024-05-27 16:19:46 +0300

    Change Inject transformer to Replace

--- hermitage/adapters/transformers.py
+++ hermitage/adapters/transformers.py
@@ -5,32 +5,33 @@ from ..notation.default import Clause
 from ..definitions.contracts.adapters import TransformerContract
 
 
-class Inject(TransformerContract):
+class Replace(TransformerContract):
     def __init__(
         self,
-        field_name: str,
-        injection_entity: typing.Any,
-        injection_field: str | None = None
+        search_field: str,
+        replace_field: str | None = None,
+        replace_entity: typing.Any = None,
     ):
-        super().__init__(field_name)
-        self._injection_field = injection_field or field_name
-        self._injection_entity = injection_entity
+        super().__init__(search_field)
+        self._replace_field = replace_field or search_field
+        self._replace_entity = replace_entity
 
     def _apply_value(self, value: typing.Any):
-        if callable(self._injection_entity):
-            return self._injection_entity(value)
+        if callable(self._replace_entity):
+            return self._replace_entity(value)
         return value
 
     def __call__(self, clause: Clause):
         operation = clause.operation
-        if isinstance(operation, zodchy.codex.query.SET):
-            operation = zodchy.codex.query.SET(
-                *[self._apply_value(v) for v in operation.value]
-            )
-        else:
-            operation = type(operation)(self._apply_value(operation.value))
+        if self._replace_entity is not None:
+            if isinstance(operation, zodchy.codex.query.SET):
+                operation = zodchy.codex.query.SET(
+                    *[self._apply_value(v) for v in operation.value]
+                )
+            else:
+                operation = type(operation)(self._apply_value(operation.value))
         clause = Clause(
-            self._injection_field,
+            self._replace_field,
             operation
         )
         return clause
--- tests/adapters/test_transformers.py
+++ tests/adapters/test_transformers.py
@@ -5,14 +5,14 @@ from hermitage.adapters import transformers
 from .definitions import GetTasks, TaskState, task_state_reference
 
 
-def test_inject_range(adapter):
+def test_replace_range(adapter):
     query = GetTasks(
         state=zodchy.codex.query.SET(TaskState.new, TaskState.done),
     )
     state_clause = None
     for clause in adapter(
         query,
-        transformers.Inject("state", task_state_reference, "state_id")
+        transformers.Replace("state", "state_id", task_state_reference)
     ):
         if clause.name == "state_id":
             state_clause = clause
